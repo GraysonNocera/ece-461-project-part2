08832904a179843ab2a2e3244d1457cde867c578
Hamzah Ayman Kamel <kamelh@eceprog4.ecn.purdue.edu>
2023-02-10 16:42:59 -0500

    Trying to Integrate license

--- Typescript/metrics.py
+++ Typescript/metrics.py
@@ -41,7 +41,7 @@ def main():
         result = get_pulls()
         open("pulls.json","w").write(json.dumps(f'{func}: {result}'))
     elif func=="get_license":
-        result = get_license()
+        result = get_license(user_id, repo)
         open("license.json","w").write(json.dumps(f'{func}: {result}'))
     else:
         result = "invalid input"
@@ -101,8 +101,28 @@ def has_downloads():
     return "5"
 def get_pulls():
     return "6"
-def get_license():
-    return "7"
+
+def get_license(user_id, repo):
+    repo_url = f"https://github.com/{user_id}/{repo}.git"
+    
+    # Clone the repository
+    git.Repo.clone_from(repo_url, repo)
+
+    license_file = os.path.join(repo, "LICENSE.txt")
+    readme_file = os.path.join(repo, "README.txt")
+
+    # Read the contents of the license and readme files
+    if os.path.exists(license_file):
+        return "1"
+    elif os.path.exists(readme_file):
+        with open(readme_file, "r") as f:
+            readme_content = f.read()
+            if "License" in readme_content or "Licensing" in readme_content or "licensing" in readme_content:
+                return "1"
+            else:
+                return "0"
+    else:
+        return "0"
 
 if __name__ == '__main__':
     main()
