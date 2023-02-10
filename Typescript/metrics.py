039da303b95c95068bd147d4af9aaa7e9de85349
Hamzah Ayman Kamel <kamelh@eceprog1.ecn.purdue.edu>
2023-02-10 14:53:13 -0500

    Close to working code

--- Typescript/metrics.py
+++ Typescript/metrics.py
@@ -9,20 +9,21 @@ import requests
 # RESPONSIVE_MAINTAINER: get_collaborators or has_downloads or get_pulls
 # LICENSE: get_license
 
-git_token = os.environ.get('GITHUB_TOKEN')
 
 def main():
-    # if(len(sys.argv) != 3):
-    #     print("Invalid length of arguments")
-    #     return 1 
+
+    if(len(sys.argv) != 3):
+        print("Invalid length of arguments")
+        return 1 
     
     func = sys.argv[1]
-    # argv2 = sys.argv[2]
-    # argv3 = sys.argv[3]
+    user = sys.argv[2]
+    repo = sys.argv[3]
+    token = os.environ.get('GITHUB_TOKEN')
+    # open("testing.json","w").write(json.dumps(f'{func}, {user}, {repo}'))
 
     if func=="get_downloads":
-        print(func)
-        result = get_downloads()
+        result = get_downloads(user, repo, token)
         open("downloads.json","w").write(json.dumps(f'{func}: {result}'))
     elif func=="get_issues":
         result = get_issues()
@@ -48,23 +49,23 @@ def main():
     # open("pyout.json","w").write(json.dumps(f'{func}: {result}'))
     # print(result) # Don't print to communicate with TS
     
-def get_downloads():
-    # num_downloads = 0
+def get_downloads(user_id, repo, git_token):
+    num_downloads = 0
 
-    # # Setting up API
-    # downloads_url = f"https://api.github.com/repos/{user_id}/{repo}/releases"
-    # headers = {"Authorization": f"{git_token}"}
+    # Setting up API
+    downloads_url = f"https://api.github.com/repos/{user_id}/{repo}/releases"
+    headers = {"Authorization": f"{git_token}"}
 
-    # downloads_request = requests.get(downloads_url, headers=headers)
-    # releases = downloads_request.json()
-    # num_releases = len(releases)
+    downloads_request = requests.get(downloads_url, headers=headers)
+    releases = downloads_request.json()
+    num_releases = len(releases)
 
-    # # Calculating total number of downloads
-    # if downloads_request.status_code == 200 and "download_count" in releases[0]["assets"][0]:
-    #     for i in range(0, num_releases - 1):
-    #         num_downloads += int(releases[i]["assets"][0]["download_count"])
+    # Calculating total number of downloads
+    if downloads_request.status_code == 200 and "download_count" in releases[0]["assets"][0]:
+        for i in range(0, num_releases - 1):
+            num_downloads += int(releases[i]["assets"][0]["download_count"])
 
-    return "1"
+    return str(num_downloads)
 
 def get_issues():
     return "2"
