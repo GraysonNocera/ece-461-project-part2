860c7bdb9f525cd81d7f975456f8f898a95cf3f2
khauersp <54869912+khauersp@users.noreply.github.com>
2023-03-18 23:44:04 -0400

    feat: replaced get_downloads with percent comment

--- metrics.py
+++ metrics.py
@@ -20,10 +20,8 @@ def main():
     if not os.path.exists("jsons/"):
         os.mkdir("jsons/")
 
-    if func == "get_downloads":
-        result = get_downloads(user, repo, token)
-        open(f"jsons/downloads{user}.json", "w").write(json.dumps(f"{func}: {result}"))
-    elif func == "get_issues":
+
+    if func == "get_issues":
         result = get_issues(user, repo, token)
         open(f"jsons/issues{user}.json", "w").write(json.dumps(f"{func}: {result}"))
     elif func == "get_forks":
@@ -61,33 +59,6 @@ def get_clone(user_id: str, repo: str) -> str:
         git.Repo.clone_from(repo_url, repo)
     except:
         return "0"
-
-
-def get_downloads(user_id: str, repo: str, git_token: str) -> str:
-    # open("error.json","w").write(json.dumps(f'get_downloads inputs: {user_id} {repo} {git_token}'))
-    num_downloads = 0
-
-    # Setting up API
-    downloads_url = f"https://api.github.com/repos/{user_id}/{repo}/releases"
-    headers = {"Authorization": f"{git_token}"}
-    downloads_request = requests.get(downloads_url, headers=headers)
-
-    releases = downloads_request.json()
-
-    num_releases = len(releases)
-
-    # Calculating total number of downloads
-    try:
-        if (
-            downloads_request.status_code == 200
-            and "download_count" in releases[0]["assets"][0]
-        ):
-            for i in range(0, num_releases - 1):
-                num_downloads += int(releases[i]["assets"][0]["download_count"])
-
-    except:
-        num_downloads = 0
-    return str(num_downloads)
 
 
 def get_issues(user_id: str, repo: str, git_token: str) -> str:
