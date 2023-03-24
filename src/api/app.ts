13f12ea81452b0f2ba6acbc4ba5ec2a7f7404136
Grayson Nocera <gnocera@purdue.edu>
2023-03-24 15:07:19 -0400

    add authentication endpoint

--- src/api/app.ts
+++ src/api/app.ts
@@ -2,7 +2,9 @@ let express = require('express');
 let path = require('path');
 import { logger } from '../logging';
 import { PackageData } from "./model/packageData";
+
 const packageRouter = require('./route/package.route');
+const authRouter = require('./route/authenticate.route');
 
 const app = express();
 
@@ -13,7 +15,10 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res, next) => {
     res.send('respond with a resource');
 });
+
+// Endpoints
 app.use('/package', packageRouter);
+app.use('/authenticate', authRouter);
 
 app.listen(3000, () => {
   logger.info("API server listening on port 3000");
