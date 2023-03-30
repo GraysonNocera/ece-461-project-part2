32a87ca63a51bd0e196c56d9525c852eb8b0c083
Grayson Nocera <gnocera@purdue.edu>
2023-03-29 23:30:59 -0400

    feat: implement reset endpoint

---
+++ src/api/route/reset.route.ts
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+import { authorizeUser } from '../middleware/authorize_user';
+import { logger } from '../../logging';
+import { Request, Response } from 'express';
+import Joi from 'joi';
+const express = require('express');
+
+const resetRouter: Router = express.Router();
+
+// This ensures that Content, URL, and JSProgram are all inputted as strings
+const schema = Joi.object({
+    Content: Joi.string(),
+    URL: Joi.string(),
+    JSProgram: Joi.string(),
+});
+
+// Create a package when DELETE /reset is called
+resetRouter.delete('/', authorizeUser, (req: Request, res: Response) =>  {
+    logger.info("DELETE /reset");
+
+    let auth: string;
+    try {
+        logger.info("Request body: " + req.headers);
+        auth = req.get('X-Authorization') || "";
+        logger.info("Auth data: " + auth);
+        // TODO: check authorization
+
+        // TODO: reset registry
+
+        // 200: reset successful
+        // 401: unauthorized
+        res.status(200).send("Registry is reset");
+
+        res.status(401).send("You do not have permission to reset the registry.");
+    } catch {
+        // Request body is not valid JSON
+        logger.info("Invalid JSON for DELETE /reset");
+    }
+
+    // Validate with joi (trivial example)
+});
+
+module.exports = resetRouter;
