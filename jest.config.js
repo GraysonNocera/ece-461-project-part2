dab6018f969348ec8fc123ae134149aa3b172688
b4yuan <89487381+b4yuan@users.noreply.github.com>
2023-04-01 17:00:16 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ["**/?(*.)+(spec|test).ts", "**/apitests.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/src/angular_autogen/"],
+  transformIgnorePatterns: ['/node_modules/(?!@ngrx|angular2-ui-switch|ng-dynamic)']
 };
