d5b635dfe62771cba9e612b07e6f705691dfaf99
b4yuan <89487381+b4yuan@users.noreply.github.com>
2023-04-01 17:55:05 -0400

    Update apitests.ts

--- testing/apitests.ts
+++ testing/apitests.ts
@@ -3,36 +3,42 @@ import axios from 'axios';
 describe('API tests', () => {
   it('should create a package', async () => {
     const response = await axios.post('http://localhost:3000/packages', {
-        name: 'test-package',
-        version: '1.0.0',
-        ID: '1234'
-    });
-
-    expect(response.status).toBe(200);
-    expect(response.data).toEqual(expect.arrayContaining([
-    expect.objectContaining({
       name: 'test-package',
       version: '1.0.0',
       ID: '1234',
-    }),
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: 'test-package',
+          version: '1.0.0',
+          ID: '1234',
+        }),
+      ]),
+    );
   });
 
   it('should get a package by name', async () => {
     const response = await axios.get('http://localhost:3000/package/byName/test-package');
 
     expect(response.status).toBe(200);
-    expect(response.data).toEqual(expect.arrayContaining([
-    expect.objectContaining({
-      name: 'test-package',
-      version: '1.0.0',
-      ID: '1234',
-    }),
+    expect(response.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: 'test-package',
+          version: '1.0.0',
+          ID: '1234',
+        }),
+      ]),
+    );
   });
 
-//   it('should get a list of packages', async () => {
-//     const response = await axios.get('http://localhost:3000/package');
+  //   it('should get a list of packages', async () => {
+  //     const response = await axios.get('http://localhost:3000/package');
 
-//     expect(response.status).toBe(200);
-//     expect(Array.isArray(response.data)).toBe(true);
-//   });
+  //     expect(response.status).toBe(200);
+  //     expect(Array.isArray(response.data)).toBe(true);
+  //   });
 });
