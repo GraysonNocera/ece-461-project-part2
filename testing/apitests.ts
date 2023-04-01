8006fbdf133bdd298af2135b6dbd4162073654ff
b4yuan <89487381+b4yuan@users.noreply.github.com>
2023-04-01 17:44:31 -0400

    Update apitests.ts

--- testing/apitests.ts
+++ testing/apitests.ts
@@ -9,22 +9,24 @@ describe('API tests', () => {
     });
 
     expect(response.status).toBe(200);
-    expect(response.data).toMatchObject({
-        name: 'test-package',
-        version: '1.0.0',
-        ID: '1234'
-    });
+    expect(response.data).toEqual(expect.arrayContaining([
+    expect.objectContaining({
+      name: 'test-package',
+      version: '1.0.0',
+      ID: '1234'
+    })
   });
 
   it('should get a package by name', async () => {
     const response = await axios.get('http://localhost:3000/package/byName/test-package');
 
     expect(response.status).toBe(200);
-    expect(response.data).toMatchObject({
-        name: 'test-package',
-        version: '1.0.0',
-        ID: '1234'
-    });
+    expect(response.data).toEqual(expect.arrayContaining([
+    expect.objectContaining({
+      name: 'test-package',
+      version: '1.0.0',
+      ID: '1234'
+    })
   });
 
 //   it('should get a list of packages', async () => {
