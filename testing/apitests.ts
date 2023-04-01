d50e32bcffe588ea225ea27561514fe50506b936
b4yuan <89487381+b4yuan@users.noreply.github.com>
2023-04-01 17:47:52 -0400

    Update apitests.ts

--- testing/apitests.ts
+++ testing/apitests.ts
@@ -13,7 +13,7 @@ describe('API tests', () => {
     expect.objectContaining({
       name: 'test-package',
       version: '1.0.0',
-      ID: '1234'
+      ID: '1234',
     })
   });
 
@@ -25,7 +25,7 @@ describe('API tests', () => {
     expect.objectContaining({
       name: 'test-package',
       version: '1.0.0',
-      ID: '1234'
+      ID: '1234',
     })
   });
 
