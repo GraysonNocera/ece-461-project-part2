7f9f76808dbf015d7241f65507c915515cbbf216
b4yuan <89487381+b4yuan@users.noreply.github.com>
2023-04-01 14:12:47 -0400

    Update app.ts

--- src/api/app.ts
+++ src/api/app.ts
@@ -4,7 +4,7 @@ import { logger } from '../logging';
 import { PackageData } from "./model/packageData";
 
 import { packageRouter } from './route/package.route';
-import { authRouter from './route/authenticate.route';
+import { authRouter } from './route/authenticate.route';
 import { packagesRouter } from './route/packages.route';
 import { resetRouter } from './route/reset.route';
 
@@ -17,13 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res, next) => {
     res.send('respond with a resource');
-});,
+});
 
 // Endpoints
 app.use('/package', packageRouter);
 app.use('/authenticate', authRouter);
 app.use('/packages', packagesRouter);
-app.use('/reset', resetRouter);,
+app.use('/reset', resetRouter);
 
 app.listen(3000, () => {
   logger.info("API server listening on port 3000");
