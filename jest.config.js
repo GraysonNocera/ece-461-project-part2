dad9a99187e554566aaba04eba2429c44ae12028
b4yuan <89487381+b4yuan@users.noreply.github.com>
2023-04-01 17:38:00 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json'
-    }
-  },
   testMatch: ["**/?(*.)+(spec|test).ts", "**/apitests.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/src/angular_autogen/"],
-  transformIgnorePatterns: ['/node_modules/(?!@angular)']
+  transformIgnorePatterns: ['/node_modules/(?!@angular)'],
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  }
 };
