7913253ec30bade310f1e069dc37a486fb775e02
khauersp <54869912+khauersp@users.noreply.github.com>
2023-04-06 00:38:42 -0400

    feat: can verify dummy user and generate token

--- src/api/route/authenticate.route.ts
+++ src/api/route/authenticate.route.ts
@@ -6,7 +6,7 @@ import Joi from 'joi';
 import { AuthenticationRequest } from '../model/authenticationRequest';
 import { AuthenticationToken } from '../model/authenticationToken';
 const express = require('express');
-
+const jwt = require("jsonwebtoken");
 export const authRouter: Router = express.Router();
 
 // THIS IS ONLY NEEDED FOR THE TOKEN REQUIREMENT, WHICH I THINK WE DROPPED
@@ -17,31 +17,35 @@ export const authRouter: Router = express.Router();
 
 // This ensures that Content, URL, and JSProgram are all inputted as strings
 const schema = Joi.object({
-    Content: Joi.string(),
-    URL: Joi.string(),
-    JSProgram: Joi.string(),
+  Content: Joi.string(),
+  URL: Joi.string(),
+  JSProgram: Joi.string(),
 });
 
 // Create a package when POST /package is called
-authRouter.put('/', authorizeUser, (req: Request, res: Response) =>  {
-    logger.info("PUT /authenticate");
+authRouter.put("/", authorizeUser, (req: Request, res: Response) => {
+  logger.info("PUT /authenticate");
 
-    let authData: AuthenticationRequest;
-    let authToken: AuthenticationToken;
-    try {
-        authData = req.body;
-        logger.info("Auth data: " + JSON.stringify(authData));
+  let authData: AuthenticationRequest;
+  let authToken: AuthenticationToken;
+  try {
+    authData = req.body;
+    logger.info("Auth data: " + JSON.stringify(authData));
 
-        // TODO: encrypt user password
-        authToken = { Token: authData.Secret.password };
-
-        res.status(200).send(authToken);
-    } catch {
-        // Request body is not valid JSON
-        logger.info("Invalid JSON for PUT /authenticate");
+    // TODO: encrypt user password
+    authToken = { Token: jwt.sign(authData.Secret.password, "secret") };
+    // authToken = { Token: authData.Secret.password };
+    if (req.body.authorized) {
+      res.status(200).send(authToken);
+    } else {
+      res.status(403).send("Authentication Failed");
     }
+  } catch {
+    // Request body is not valid JSON
+    logger.info("Invalid JSON for PUT /authenticate");
+  }
 
-    // Validate with joi (trivial example)
+  // Validate with joi (trivial example)
 });
 
 // module.exports = authRouter;
