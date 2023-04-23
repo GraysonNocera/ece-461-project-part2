ab9b0f7ad25e5b341075702b4f1c7ed48d8d5158
Grayson Nocera <gnocera@purdue.edu>
2023-04-23 16:55:36 -0400

    style: autoformat code

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,103 +1,109 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-import mongoose from 'mongoose';
-import { logger } from '../logging';
-import * as fs from 'fs';
+const { MongoClient, ServerApiVersion } = require("mongodb");
+import mongoose from "mongoose";
+import { logger } from "../logging";
+import * as fs from "fs";
 import path from "path";
 
 let bucket: mongoose.mongo.GridFSBucket;
 export async function connectToMongo() {
-    /*
-    * This function connects to the MongoDB database
-    * It should be called before any database operations are performed
-    */
+  /*
+   * This function connects to the MongoDB database
+   * It should be called before any database operations are performed
+   */
 
-    logger.info("connectToMongo(): Connecting to MongoDB...");
+  logger.info("connectToMongo(): Connecting to MongoDB...");
 
-    // Set the following environment variables
-    const USERNAME: string = process.env.MONGODB_USERNAME || "";
-    const PASSWORD: string = process.env.MONGODB_PASSWORD || "";
+  // Set the following environment variables
+  const USERNAME: string = process.env.MONGODB_USERNAME || "";
+  const PASSWORD: string = process.env.MONGODB_PASSWORD || "";
 
-    // Keep this as "database"
-    // We connect to the database, which will hold a bunch of collections
-    // Probably one collection for each file in models/ 
-    // Those collections will hold a bunch of documents
-    const DATABASE: string = "database";
-    const uri = `mongodb://${USERNAME}:${PASSWORD}@ac-buchowe-shard-00-00.bcmdhkp.mongodb.net:27017,ac-buchowe-shard-00-01.bcmdhkp.mongodb.net:27017,ac-buchowe-shard-00-02.bcmdhkp.mongodb.net:27017/${DATABASE}?ssl=true&replicaSet=atlas-zpmc7p-shard-0&authSource=admin&retryWrites=true&w=majority`
-    
-    await mongoose.connect(uri);
+  // Keep this as "database"
+  // We connect to the database, which will hold a bunch of collections
+  // Probably one collection for each file in models/
+  // Those collections will hold a bunch of documents
+  const DATABASE: string = "database";
+  const uri = `mongodb://${USERNAME}:${PASSWORD}@ac-buchowe-shard-00-00.bcmdhkp.mongodb.net:27017,ac-buchowe-shard-00-01.bcmdhkp.mongodb.net:27017,ac-buchowe-shard-00-02.bcmdhkp.mongodb.net:27017/${DATABASE}?ssl=true&replicaSet=atlas-zpmc7p-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
-    //creating bucket
-    var db = mongoose.connections[0].db;
-    bucket = new mongoose.mongo.GridFSBucket(db, {
-        bucketName: "Content"
-    });
+  await mongoose.connect(uri);
 
-    logger.info("connectToMongo(): Connected to MongoDB");
+  //creating bucket
+  var db = mongoose.connections[0].db;
+  bucket = new mongoose.mongo.GridFSBucket(db, {
+    bucketName: "Content",
+  });
+
+  logger.info("connectToMongo(): Connected to MongoDB");
 }
 
 export async function disconnectFromMongo() {
-    /*
-    * This function disconnects from the MongoDB database
-    */
+  /*
+   * This function disconnects from the MongoDB database
+   */
 
-    logger.info("disconnectFromMongo(): Disconnecting from MongoDB...");
+  logger.info("disconnectFromMongo(): Disconnecting from MongoDB...");
 
-    mongoose.connection.close();
+  mongoose.connection.close();
 
-    logger.info("disconnectFromMongo(): Disconnected from MongoDB");
+  logger.info("disconnectFromMongo(): Disconnected from MongoDB");
 }
 
-export async function uploadFileToMongo(filePath: string, fileName: string, id: mongoose.Types.ObjectId) {
+export async function uploadFileToMongo(
+  filePath: string,
+  fileName: string,
+  id: mongoose.Types.ObjectId
+) {
+  logger.info("uploadFileToMongo(): Uploading file to MongoDB: " + fileName);
 
-    logger.info("uploadFileToMongo(): Uploading file to MongoDB: " + fileName);
+  let stream = bucket.openUploadStream(fileName);
+  stream.id = id;
 
-    let stream = bucket.openUploadStream(fileName);
-    stream.id = id;
-    
-    fs.createReadStream(filePath).pipe(stream).
-    on('error', function(error) {
-        logger.debug("Error in inserting file: " + error);
-    }).
-    on('finish', function() {
-        logger.info("File Inserted into mongo, deleting it locally");
-        fs.rm(filePath, function(err) {
-            if (err) {
-                logger.debug("Error in deleting file: " + err);
-            }
-        });
+  fs.createReadStream(filePath)
+    .pipe(stream)
+    .on("error", function (error) {
+      logger.debug("Error in inserting file: " + error);
+    })
+    .on("finish", function () {
+      logger.info("File Inserted into mongo, deleting it locally");
+      fs.rm(filePath, function (err) {
+        if (err) {
+          logger.debug("Error in deleting file: " + err);
+        }
+      });
     });
 }
 
-export async function downloadFileFromMongo(id: mongoose.Types.ObjectId, callback: Function) {
+export async function downloadFileFromMongo(
+  id: mongoose.Types.ObjectId,
+  callback: Function
+) {
+  let content: string;
+  let filePath: string;
 
-    let content: string;
-    let filePath: string;
+  filePath = path.join(__dirname, "..", "artifacts", id.toString());
 
-    filePath = path.join(__dirname, '..', 'artifacts', id.toString());
+  bucket
+    .openDownloadStream(id)
+    .pipe(fs.createWriteStream(filePath))
+    .on("error", function (error) {
+      logger.debug("Error in downloading file: " + error);
+      callback(null, error);
+    })
+    .on("finish", function () {
+      logger.info("File Downloaded");
+      content = fs.readFileSync(filePath, "base64");
+      fs.rm(filePath, function (err) {
+        if (err) {
+          logger.debug("Error in deleting file: " + err);
+          callback(null, err);
+        }
+      });
 
-    bucket.openDownloadStream(id).
-        pipe(fs.createWriteStream(filePath)).
-        on('error', function(error) {
-            logger.debug("Error in downloading file: " + error);
-            callback(null, error);
-        }).
-        on('finish', function() {
-            logger.info("File Downloaded");
-            content = fs.readFileSync(filePath, 'base64');
-            fs.rm(filePath, function(err) {
-                if (err) {
-                    logger.debug("Error in deleting file: " + err);
-                    callback(null, err);
-                }
-            });
-
-            callback(content, null);
-        });
+      callback(content, null);
+    });
 }
 
 export async function deleteFileFromMongo(id: mongoose.Types.ObjectId) {
+  logger.info("deleteFileFromMongo(): Deleting file from MongoDB: " + id);
 
-    logger.info("deleteFileFromMongo(): Deleting file from MongoDB: " + id);
-
-    bucket.delete(id);
+  bucket.delete(id);
 }
