c58d198aaf1f0caa670ef8c3b1e04ea64ea9ca6a
Brian Yuan <bayuan@purdue.edu>
2023-04-23 16:38:11 -0400

    bhye b yegit add .git add .git add .

--- src/angular_autogen/src/app/app-routing.module.ts
+++ src/angular_autogen/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HelpComponent } from './help/help.component';
-import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [
-  { path: '', component: PostsComponent },
-  { path: 'help', component: HelpComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
