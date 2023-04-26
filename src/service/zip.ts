76382d16faf56498beefb03fd7beee270baba325
Grayson Nocera <gnocera@purdue.edu>
2023-04-26 15:19:04 -0400

    feat: get the readme

--- src/service/zip.ts
+++ src/service/zip.ts
@@ -1,8 +1,8 @@
-import JSZip from "jszip";
+import JSZip, { files } from "jszip";
 import { Octokit as OctokitType } from "octokit";
 import * as fs from "fs/promises";
-const AdmZip = require('adm-zip');
-import { Buffer } from 'buffer';
+const AdmZip = require("adm-zip");
+import { Buffer } from "buffer";
 import * as path from "path";
 const Octokit = OctokitType as any;
 import { getGitRepoDetails } from "./misc";
@@ -14,7 +14,6 @@ const octokit = new Octokit({
 
 // Convert the zip file pulled from github to base64 string
 async function zipToBase64(filePath: string): Promise<string> {
-
   logger.info("zipToBase64: Converting zip file to base64 string");
   const zipFile = await fs.readFile(filePath);
   const zip = await JSZip.loadAsync(zipFile);
@@ -24,8 +23,12 @@ async function zipToBase64(filePath: string): Promise<string> {
 }
 
 async function getPackageZip(owner: string, repo: string) {
-
-  logger.info("Downloading package zip from github with owner: " + owner + " and repo: " + repo);
+  logger.info(
+    "Downloading package zip from github with owner: " +
+      owner +
+      " and repo: " +
+      repo
+  );
 
   let result = await octokit.request(`GET /repos/${owner}/${repo}/zipball`, {
     owner: owner,
@@ -40,7 +43,10 @@ async function getPackageZip(owner: string, repo: string) {
     new Array(result.data)
   );
 
-  logger.info("Package zip downloaded successfully, wrote to: " + path.join(__dirname, "..", "artifacts", `${repo}.zip`));
+  logger.info(
+    "Package zip downloaded successfully, wrote to: " +
+      path.join(__dirname, "..", "artifacts", `${repo}.zip`)
+  );
 }
 
 export async function getContentFromUrl(url: string): Promise<string | null> {
@@ -56,7 +62,7 @@ export async function getContentFromUrl(url: string): Promise<string | null> {
     return null;
   }
 
-  logger.info("getContentFromUrl: Getting package zip from github")
+  logger.info("getContentFromUrl: Getting package zip from github");
   await getPackageZip(details.username, details.repoName);
 
   let zipFilePath: string = path.join(
@@ -73,10 +79,12 @@ export async function getContentFromUrl(url: string): Promise<string | null> {
     `${details.repoName}.txt`
   );
 
-  logger.info("getContentFromUrl: Converting zip file to base64 string")
+  logger.info("getContentFromUrl: Converting zip file to base64 string");
   let content = await zipToBase64(zipFilePath);
 
-  logger.info("getContentFromUrl: Writing base64 string to file " + txtFilePath)
+  logger.info(
+    "getContentFromUrl: Writing base64 string to file " + txtFilePath
+  );
   fs.writeFile(txtFilePath, content);
   fs.rm(zipFilePath);
 
@@ -84,10 +92,9 @@ export async function getContentFromUrl(url: string): Promise<string | null> {
 }
 
 async function unzipContent(content: string) {
-
   logger.info("unzipContent: Unzipping content");
 
-  const buffer = Buffer.from(content, 'base64');
+  const buffer = Buffer.from(content, "base64");
   const zip = new AdmZip(buffer);
   let basePath = path.join(__dirname, "..", "artifacts", "unzipped");
 
@@ -100,28 +107,68 @@ async function unzipContent(content: string) {
   // });
   // let certain_file = zip.readAsText(zipEntries[1].entryName);
   // console.log(certain_file)
-  
+
   await zip.extractAllTo(basePath, true);
 
   return basePath;
 }
 
-export async function getPackageJSON(content: string): Promise<Object> {
-  logger.info("getPackageJSON: Getting url from content base64 string");
+export async function getInfoFromContent(content: string): Promise<{ package_json: Object, readme: string }> {
+  logger.info("getInfoFromContent: Getting package_json and readme");
 
   let basePath = await unzipContent(content);
+
+  let package_json = await getPackageJSON(basePath);
+  let readme = await getReadme(basePath);
+
+  fs.rm(basePath, { recursive: true });
+
+  return { package_json, readme };
+}
+
+async function getReadme(basePath: string): Promise<string> {
+  logger.info("getReadme: Getting readme from content base64 string");
+
+  // Search for string case insensitive
+  let readme_regex: RegExp = /readme\..+/i;
+  let readme_name: string = "";
+
+  let files = await fs.readdir(basePath)
+  await Promise.all(files.map((file) => {
+    if (readme_regex.test(file)) {
+      readme_name = file;
+    }
+  }));
+
+  if (!readme_name) {
+    logger.debug("getReadme: No readme found");
+    return "";
+  }
+
+  let readme_path: string = path.join(basePath, readme_name);
+  let readme_data: string = await fs.readFile(readme_path, "utf8");
+
+  logger.info("getReadme: Found readme");
+
+  return readme_data.substring(0, 150);
+}
+
+async function getPackageJSON(basePath: string): Promise<Object> {
+  logger.info("getPackageJSON: Getting url from content base64 string");
 
   logger.info("getPackageJSON: Read base64 string into zip file");
 
   let package_json_path: string = path.join(basePath, "package.json");
-  let package_json_contents: string = await fs.readFile(package_json_path, "utf8");
+  let package_json_contents: string = await fs.readFile(
+    package_json_path,
+    "utf8"
+  );
 
   let package_json_object: Object;
   if (package_json_contents) {
     package_json_object = JSON.parse(package_json_contents);
     if (package_json_object) {
       logger.info("getPackageJSON: Found package.json");
-      fs.rm(basePath, { recursive: true });
       return package_json_object;
     }
     logger.debug("getPackageJSON: Unable to parse package.json");
