94b8882be1556d62fa225634b63fcd83d8e17cdf
Grayson Nocera <gnocera@purdue.edu>
2023-04-29 12:46:31 -0400

    test: begin adding rate test

--- testing/rate.test.ts
+++ testing/rate.test.ts
@@ -7,30 +7,36 @@ describe("Test the rate module", () => {
   test("Test the rate module", () => {
     let url = "https://github.com/facebook/react";
     cp.execSync(
-      `npx ts-node ${path.join(__dirname, "..", "src", "rate", "hello-world.ts")} ${url}`
+      `npx ts-node ${path.join(
+        __dirname,
+        "..",
+        "src",
+        "rate",
+        "hello-world.ts"
+      )} ${url}`
     );
 
-    let filePath: string = path.join(__dirname, "..", "src", "rate", "score.json")
-    const packageRate: PackageRating = JSON.parse(readFileSync(filePath, 'utf8'));
-    const values = Object.entries(packageRate)
-      .filter(([key]) => key !== 'NetScore')
-      .map(([, value]) => value);
-    const averageScore = average(values);
-    expect(averageScore).toBeCloseTo(packageRate.NetScore)
+    let filePath: string = path.join(
+      __dirname,
+      "..",
+      "src",
+      "rate",
+      "score.json"
+    );
+    const packageRate: PackageRating = JSON.parse(
+      readFileSync(filePath, "utf8")
+    );
+    const averageScore = weightedAverage(packageRate);
+    expect(averageScore).toBeCloseTo(packageRate.NetScore);
   });
 });
 
-function average(arr: number[]) {
-  let sum = 0;
-  arr.forEach((element) => {
-    sum += element;
-  });
-  return sum / arr.length;
-}
-
 function weightedAverage(packageRate: PackageRating): Number {
-
-  const average = packageRate.LicenseScore * 0.2 + packageRate.BusFactor * 
+  const average =
+    packageRate.LicenseScore * 0.2 +
+    packageRate.BusFactor * 0.2 * packageRate.GoodPinningPractice * 0.1 +
+    packageRate.GoodEngineeringPractice * 0.1 + 
+    packageRate. * 0.1 +;
 
   return 0;
 }
